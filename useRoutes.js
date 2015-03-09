0888a532e14996a30fcd8b5f56f14b260623b4fb
unknown <842450138@qq.com>
2015-03-09 16:34:29 +0800

    change ueditor server to ueditor-nodejs,support bcs and local storage

--- useRoutes.js
+++ useRoutes.js
@@ -1,33 +1,6 @@
 function use (app) {
     var path = require('path');
-    var ueditor = require('./common/ueditor');
-// /ueditor 入口地址配置 https://github.com/netpi/ueditor/blob/master/example/public/ueditor/ueditor.config.js
-// 官方例子是这样的 serverUrl: URL + "php/controller.php"
-// 我们要把它改成 serverUrl: URL + 'ue'
-app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res, next) {
-  // ueditor 客户发起上传图片请求
-  if(req.query.action === 'uploadimage'){
-    // 这里你可以获得上传图片的信息
-    var foo = req.ueditor;
-    console.log(foo.filename); // exp.png
-    console.log(foo.encoding); // 7bit
-    console.log(foo.mimetype); // image/png
-    // 下面填写你要把图片保存到的路径 （ 以 path.join(__dirname, 'public') 作为根路径）
-    var img_url = '/uploadimage/';
-    res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做
-  }
-  //  客户端发起图片列表请求
-  else if (req.query.action === 'listimage'){
-    var dir_url = '/uploadimage/'; // 要展示给客户端的文件夹路径
-    res.ue_list(dir_url) // 客户端会列出 dir_url 目录下的所有图片
-  }
-  // 客户端发起其它请求
-  else {
-
-    res.setHeader('Content-Type', 'application/json');
-    // 这里填写 ueditor.config.json 这个文件的路径
-    res.redirect('/ueditor/nodejs/config.json')
-}}));
+    var ueditor = require('ueditor-nodejs');
     var routes = require('./routes/index');
     var login = require('./routes/login');
     var doLogin = require('./routes/doLogin');
@@ -44,6 +17,15 @@ app.use("/ueditor/ue", ueditor(path.join(__dirname, 'public'), function(req, res
     var edit = require('./routes/blog/edit');
     var doEdit = require('./routes/blog/doEdit');
     var admin = require('./routes/admin');
+
+    app.use('/ueditor/ue', ueditor({
+        configFile: '/ueditor/nodejs/config.json',
+        mode: 'local',
+        AccessKey: 'AdGlhYkMA1khQ1jq3uZMZF14',
+        SecrectKey: 'oiUqt1VpH3fddPSWmRWjDwTIWudSMxof',
+        staticPath: path.join(__dirname, 'public'),
+        dynamicPath: '/uploadimage'
+    }));
     app.use('/', routes);
     app.use('/login', login);
     app.use('/doLogin', doLogin);
